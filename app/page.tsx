fc62c8d340e1f4be6915a10e6e7120dcda84febb
Edubuk <edubuk.trust@gmail.com>
2024-03-21 20:55:42 +0530

    Update page.tsx
    Updated text on the home page of the front end of the dApp

--- app/page.tsx
+++ app/page.tsx
@@ -187,7 +187,7 @@ export default function Home() {
 
         <p className="w-4/5 max-sm:pt-8 sm:pt-12 lg:pt-16 lg:px-14 max-w-5xl max-sm:text-lg sm:text-xl max-md:text-xl md:text-2xl">
           Edubuk&apos;s patent published model is globally award winning and recognized by various reputed organizations including G20 Summit in Indonesia (2022).
-          Best Edtech Startup Jury’s choice out of 100 Startups, CNN, Financial Times, MIT University, Harvard University in the US, World Economic Forum (Davos, Jan 2023), AWS Edstart, IIT Bombay, IIT Kharagpur, IIM Calcutta, Polygon Blockchain, Cardano Blockchain, The HINDU, CNBC, MoneyControl, RaiseMoney, IDFC-First Bank, Inc. 42, NASSCOM, State Government of Telangana (India), Dubai Expo 2020, TiE Delhi, TiE Mumbai, Data Innovation Bazaar, Telangana AI-Mission, Bombay Stock Exchange&apos;s Impact Startups, TimesNext, European Digital University, among many others.
+          Best Edtech Startup Jury’s choice out of 100 Startups, CNN, Financial Times, MIT University, Harvard University in the US, World Economic Forum (Davos, Jan 2023), AWS Edstart, IIT Bombay, IIT Kharagpur, IIM Calcutta, Polygon Blockchain, Concordium Blockchain, The HINDU, CNBC, MoneyControl, RaiseMoney, IDFC-First Bank, Inc. 42, NASSCOM, State Government of Telangana (India), Dubai Expo 2020, TiE Delhi, TiE Mumbai, Data Innovation Bazaar, Telangana AI-Mission, Bombay Stock Exchange&apos;s Impact Startups, TimesNext, European Digital University, among many others.
         </p>
 
         <ConveyorBeltAwards />
